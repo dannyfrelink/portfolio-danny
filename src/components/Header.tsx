7987067abe48e067f2148357751a1a9382807dc3
dannyfrelink <danny-frelink@hotmail.com>
2024-07-09 16:00:14 +0200

    fix: change lg: to xl: on Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
   return (
-    <header className="w-full border-b-[0.5px] border-b-textColor flex justify-between items-center px-[7.5vw] py-[14px] sm:border-b-[0.75px] lg:py-4">
+    <header className="w-full border-b-[0.5px] border-b-textColor flex justify-between items-center px-[7.5vw] py-[14px] sm:border-b-[0.75px] xl:py-4">
       <div className="h-[42px] w-[42px] bg-secondary rounded-2xl"></div>
 
-      <ul className="flex gap-4 sm:gap-6 lg:gap-8">
+      <ul className="flex gap-4 sm:gap-6 xl:gap-8">
         <li>About</li>
         <li>Portfolio</li>
         <li>Contact</li>
