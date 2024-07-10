ad14ae1bee7b43f572b53db7e3e3d168926e8bf2
dannyfrelink <danny-frelink@hotmail.com>
2024-07-10 12:42:31 +0200

    featL create component for Contact info items

---
+++ src/components/Contact.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import React from "react";
+import BaseText from "./typography/BaseText";
+
+interface ContactProps {
+  contact: {
+    content: string;
+    href?: string;
+    icon: string;
+  };
+}
+
+const Contact: React.FC<ContactProps> = ({ contact }) => {
+  const icon = require(`../assets/contact-icons/${contact.icon}`);
+
+  return (
+    <article className="flex items-center gap-3 sm:gap-5">
+      <Image
+        className="h-4 w-auto sm:h-5"
+        src={icon}
+        alt={`Info ${contact.icon.split(".")[0]}`}
+        width={100}
+        height={100}
+      />
+
+      {contact.href ? (
+        <a
+          className="block border-b-[0.5px] border-textColor sm:text-lg sm:border-b-[0.75px]"
+          href={contact.href}
+          target="_blank"
+        >
+          {contact.content}
+        </a>
+      ) : (
+        <BaseText>{contact.content}</BaseText>
+      )}
+    </article>
+  );
+};
+
+export default Contact;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Button from "@/components/Button";
+import Contact from "@/components/Contact";
 import Header from "@/components/Header";
 import Project from "@/components/Project";
 import Skill from "@/components/Skill";
@@ -130,32 +131,9 @@ export default function Home() {
           <H2>Contact</H2>
 
           <div className="[&>*:first-child]:mt-8 [&>*:not(:first-child)]:mt-6 sm:flex sm:flex-wrap sm:max-w-[750px] sm:[&>*]:w-1/2 sm:[&>*]:!mt-9">
-            {contactInfo.map((contact) => {
-              const icon = require(`../assets/contact-icons/${contact.icon}`);
-              return (
-                <article className="flex items-center gap-3 sm:gap-5">
-                  <Image
-                    className="h-4 w-auto sm:h-5"
-                    src={icon}
-                    alt={`Info ${contact.icon.split(".")[0]}`}
-                    width={100}
-                    height={100}
-                  />
-
-                  {contact.href ? (
-                    <a
-                      className="block border-b-[0.5px] border-textColor sm:text-lg sm:border-b-[0.75px]"
-                      href={contact.href}
-                      target="_blank"
-                    >
-                      {contact.content}
-                    </a>
-                  ) : (
-                    <BaseText>{contact.content}</BaseText>
-                  )}
-                </article>
-              );
-            })}
+            {contactInfo.map((contact) => (
+              <Contact key={contact.icon.split(".")[0]} contact={contact} />
+            ))}
           </div>
         </section>
       </footer>
